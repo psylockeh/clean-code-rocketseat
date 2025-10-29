723ba2b91b548b08c5d8dcff5997f82d180558d9
DEBORA NOGUEIRA DA SILVA <debora.silva@ebdgrupo.com.br>
2025-10-29 09:54:06 -0300

    refactor: renomear variáveis para  seguir boas práticas de Clean Code

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,55 +1,51 @@
 // Nomenclatura de variáveis
 
-const list = [
+const githubUserCategories = [
   {
-    title: 'User',
-    followers: 5
+    categoryName: 'User',
+    minFollowersRequired: 5
   },
   {
-    title: 'Friendly',
-    followers: 50,
+    categoryName: 'Friendly',
+    minFollowersRequired: 50,
   },
   {
-    title: 'Famous',
-    followers: 500,
+    categoryName: 'Famous',
+    minFollowersRequired: 500,
   },
   {
-    title: 'Super Star',
-    followers: 1000,
+    categoryName: 'Super Star',
+    minFollowersRequired: 1000,
   },
 ]
 
-export default async function getData(req, res) {
-  const github = String(req.query.username)
+export default async function classifyGithubUser(req, res) {
+  const githubUsername = String(req.query.username)
 
-  if (!github) {
+  if (!githubUsername) {
     return res.status(400).json({
       message: `Please provide an username to search on the github API`
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const response = await fetch(`https://api.github.com/users/${githubUsername}`);
 
   if (response.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
+      message: `User with username "${githubUsername}" not found`
     })
   }
 
-  const data = await response.json()
+  const githubUserData = await response.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const sortedUserCategories = githubUserCategories.sort((a, b) =>  b.minFollowersRequired - a.minFollowersRequired); 
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const userCategory = sortedUserCategories.find(i => githubUserData.minFollowersRequired > i.minFollowersRequired)
+  
+ const userWithCategory = {
+    githubUsername,
+    category: userCategory.categoryName,
+  };
 
-  const result = {
-    github,
-    category: category.title
-  }
-
-  return result
+  return userWithCategory;
 }
-
-getData({ query: {
-  username: 'josepholiveira'
-}}, {})
